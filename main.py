ca8d8362ba1e889c2f1fd8d89190852d2497175f
Vanilla <osu_Vanilla@126.com>
2022-03-24 11:04:37 +0800

    feat[main.py]: With partial and requests hooks, check status.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from functools import partial
 
 import requests
 from requests import Response
@@ -6,15 +7,21 @@ from requests import Response
 from api_urls import ApiUrls
 from secrets import Secrets
 
+
+# https://alexwlchan.net/2017/10/requests-hooks/
+def check_for_errors(resp, *args, **kwargs):
+    resp_json = resp.json()
+    if not resp_json['status']:
+        sys.exit(resp_json)
+
+
+requests_post = partial(requests.post, hooks={'response': check_for_errors})
+
 if __name__ == '__main__':
     # https://docs.python-requests.org/zh_CN/latest/user/quickstart.html
     data = {'uname': Secrets.uname, 'password': Secrets.get_pass_b64(), 't': 'true'}
-    r: Response = requests.post(ApiUrls.fanyalogin, data=data)
-
-    r_json = r.json()
-    if not r_json['status']:
-        sys.exit(r_json)
+    r: Response = requests_post(ApiUrls.fanyalogin, data=data)
 
     r_cookies = r.cookies
-    r: Response = requests.post(ApiUrls.getNoticeCount, cookies=r_cookies)
+    r: Response = requests_post(ApiUrls.getNoticeCount, cookies=r_cookies)
     print(r.json())
