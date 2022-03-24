00a2407a9eb50614f1c9ae5c984764c3b268182f
Vanilla <osu_Vanilla@126.com>
2022-03-24 10:24:12 +0800

    style[secrets.py]: make password field private, only b64 needed.

--- secrets.py
+++ secrets.py
@@ -8,11 +8,11 @@ USER: Final[str] = 'akisamu'
 
 class Secrets:
     uname: Final[str] = open(f'{SECRET_DIR}/chaoxing/{USER}/username').read()
-    password: Final[str] = open(f'{SECRET_DIR}/chaoxing/{USER}/password').read()
+    __password: Final[str] = open(f'{SECRET_DIR}/chaoxing/{USER}/password').read()
 
     @classmethod
     def get_pass_b64(cls):
         # https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
-        password_bytes: bytes = cls.password.encode('ascii')
+        password_bytes: bytes = cls.__password.encode('ascii')
         base64_bytes = base64.b64encode(password_bytes)
         return base64_bytes.decode("ascii")
