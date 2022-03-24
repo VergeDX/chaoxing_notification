ebe58fd4524866140d26e84425d2301747aa6e7d
Vanilla <osu_Vanilla@126.com>
2022-03-24 10:20:30 +0800

    feat[secrets.py]: Read secrets without saving in file.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from secrets import Secrets
+
 if __name__ == '__main__':
-    pass
+    # https://docs.python-requests.org/zh_CN/latest/user/quickstart.html
+    # r: Response = requests.post(ApiUrls.fanyalogin, data={'key': 'value'})
+
+    print(Secrets.uname)
+    print(Secrets.password)
+    print(Secrets.get_pass_b64())
---
+++ secrets.py
@@ -0,0 +1,18 @@
+import base64
+from typing import Final
+
+# https://github.com/Mic92/sops-nix
+SECRET_DIR: Final[str] = '/run/secrets'
+USER: Final[str] = 'akisamu'
+
+
+class Secrets:
+    uname: Final[str] = open(f'{SECRET_DIR}/chaoxing/{USER}/username').read()
+    password: Final[str] = open(f'{SECRET_DIR}/chaoxing/{USER}/password').read()
+
+    @classmethod
+    def get_pass_b64(cls):
+        # https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
+        password_bytes: bytes = cls.password.encode('ascii')
+        base64_bytes = base64.b64encode(password_bytes)
+        return base64_bytes.decode("ascii")
