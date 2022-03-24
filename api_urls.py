ef63adb7a83b8f66414bbd01788de7fb97f03dc8
Vanilla <osu_Vanilla@126.com>
2022-03-24 10:31:11 +0800

    feat[main.py]: Making simple post and show text.

---
+++ api_urls.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+
+class ApiUrls:
+    fanyalogin: Final[str] = 'https://passport2.chaoxing.com/fanyalogin'
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import requests
+from requests import Response
+
+from api_urls import ApiUrls
 from secrets import Secrets
 
 if __name__ == '__main__':
     # https://docs.python-requests.org/zh_CN/latest/user/quickstart.html
-    # r: Response = requests.post(ApiUrls.fanyalogin, data={'key': 'value'})
+    data = {'uname': Secrets.uname, 'password': Secrets.get_pass_b64(), 't': 'true'}
+    r: Response = requests.post(ApiUrls.fanyalogin, data=data)
 
-    print(Secrets.uname)
-    print(Secrets.password)
-    print(Secrets.get_pass_b64())
+    print(r.text)
